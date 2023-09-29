44e7d477019a7c074e54e1f100d8a0239dba6d08
PK <kuehn@peasec.tu-darmstadt.de>
2023-09-29 21:37:32 +0200

    modifying status bar and general update

--- home/phil/features/productivity/neomutt.nix
+++ home/phil/features/productivity/neomutt.nix
@@ -1,21 +1,73 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, config, ... }:
 let
+  cat = "${pkgs.coreutils}/bin/cat";
+  w3m = "${pkgs.w3m}/bin/w3m";
+
   khard = "${pkgs.khard}/bin/khard";
+
+  alacritty = "${pkgs.alacritty}/bin/alacritty";
+  neomutt = "${pkgs.neomutt}/bin/neomutt";
+
+  home = "${config.home.homeDirectory}";
 in
 {
+
+  home.packages = with pkgs; [
+    mailcap
+  ];
+
+  xdg.configFile."mailcap".text = ''
+      text/plain; ${cat} %s; copiousoutput;
+      text/html; ${w3m} -I %{charset} -T text/html; copiousoutput;
+
+      image/*; mkdir -p /tmp/mutt \; cp %s /tmp/mutt \; xdg-open /tmp/mutt/$(basename %s) &
+
+      *; xdg-open %s &> /dev/null &;
+  '';
+
   programs.neomutt = {
     enable = true;
-    vimKeys = true;
+    #vimKeys = true;
     checkStatsInterval = 60;
+
     sidebar = {
       enable = true;
-      width = 30;
+      width = 20;
+      format = " %D%* %?N?%N/?%S";
     };
+
     settings = {
-      mark_old = "no";
-      text_flowed = "yes";
-      reverse_name = "yes";
+      color_directcolor = "yes";
+
+      menu_scroll = "yes";
+
+      pager_stop = "yes";
+      markers = "no";
+
+      sort_aux = "reverse-last-date-received";
       query_command = ''"${khard} email --parsable '%s'"'';
+
+      forward_quote = "yes";
+      fast_reply = "yes";
+      
+      sig_dashes = "no";
+      sig_on_top = "yes";
+
+      postpone = "ask-yes";
+      
+      abort_noattach = "ask-yes";
+      abort_noattach_regex = ''"\\<(anhängen|angehängt|anhang|anhänge|hängt an|anbei|attach|attached|attachments|append)\\>"'';
+
+      mailcap_path = "${home}/.config/mailcap";
+      edit_headers   = "yes";
+
+      date_format    = ''"%F, %T"'';
+      index_format   = ''"%S [%{%m-%d}]  %-15.15F  %s"'';
+      compose_format = ''"%>-(~size: %l, Atts: %a)-"'';
+      status_format  = ''"-%?V?(Limit: %V)?-%>-(%P)-"'';
+      attach_format  = ''"%I %t%2n %T%.40d%> [%.15m/%.10M, %?C?%C, ?%s]"'';
+      pager_format   = ''"-(%C/%m)-%>-(%P)"'';
+      forward_format = ''"Fwd: %s"'';
     };
 
     binds = [
@@ -38,6 +90,11 @@ in
 
       # index
       {
+        action = "sync-mailbox";
+        key = "<space>";
+        map = [ "index" ];
+      }
+      {
         action = "noop";
         key = "x";
         map = [ "index" ];
@@ -48,21 +105,54 @@ in
         map = [ "index" ];
       }
       {
-        action = "top";
+        action = "first-entry";
         key = "gg";
         map = [ "index" ];
       }
       {
-        action = "bottom";
+        action = "last-entry";
         key = "G";
         map = [ "index" ];
       }
       {
-        action = "exit";
-        key = "q";
+        action = "collapse-thread";
+        key = "-";
         map = [ "index" ];
       }
 
+      # pager
+      {
+        action = "next-line";
+        key = "j";
+        map = [ "pager" ];
+      }
+      {
+        action = "previous-line";
+        key = "k";
+        map = [ "pager" ];
+      }
+      {
+        action = "noop";
+        key = "g";
+        map = [ "pager" ];
+      }
+      {
+        action = "top";
+        key = "gg";
+        map = [ "pager" ];
+      }
+      {
+        action = "bottom";
+        key = "G";
+        map = [ "pager" ];
+      }
+      {
+        action = "exit";
+        key = "q";
+        map = [ "pager" ];
+      }
+
+      # index and pager
       {
         action = "group-reply";
         key = "R";
@@ -84,16 +174,12 @@ in
         map = [ "index" "pager" ];
       }
 
+      # editor
       {
         action = "complete-query";
         key = "<Tab>";
         map = [ "editor" ];
       }
-      #{
-      #  action = "complete";
-      #  key = "^T";
-      #  map = [ "editor" ];
-      #}
     ];
 
     macros = [
@@ -102,39 +188,80 @@ in
         key = "A";
         map = [ "index" ];
       }
+      {
+        action = "<save-entry><kill-line>~/tmp/";
+        key = "S";
+        map = [ "attach" ];
+      }
+      {
+        action = "<sync-mailbox><enter-command>source ${home}/.config/neomutt/audacis<enter><change-folder>!<enter>";
+        key = "<f2>";
+        map = [ "index" "pager" ];
+      }
+      {
+        action = "<sync-mailbox><enter-command>source ${home}/.config/neomutt/personalvorstand<enter><change-folder>!<enter>";
+        key = "<f3>";
+        map = [ "index" "pager" ];
+      }
+      #{
+      #  action = "<enter-command>source ${home}/.config/neomutt/peasec<enter>><enter>";
+      #  key = "<f4>";
+      #  map = [ "index" ];
+      #}
     ];
 
     changeFolderWhenSourcingAccount = true;
 
-    settings = {
-      date_format    = "%F, %T";
-      index_format   = "%S [%{%m-%d}]  %-15.15F  %s";
-      compose_format = "%>-(~size: %l, Atts: %a)-";
-      status_format  = "-%?V?(Limit: %V)?-%>-(%P)-";
-      attach_format  = "%I %t%2n %T%.40d%> [%.15m/%.10M, %?C?%C, ?%s]";
-      pager_format   = "-(%C/%m)-%>-(%P)";
-      sidebar_format = " %D%* %?N?%N/?%S";
-      forward_format = "Fwd: %s";
-    };
+    extraConfig = let inherit (config.colorscheme) colors; in ''
+      color normal       #${colors.base05}   default
+      color error        #${colors.base0F}   default
+      color tilde        #${colors.base0D}   default
+      color message      #${colors.base05}   default
+      color markers      #${colors.base0A}   default
+      color attachment   #${colors.base0A}   default
+      color search       #${colors.base05}   default
+      color status       #${colors.base09}   default
+      color indicator    #${colors.base09}   black
+      color tree         #${colors.base05}   default
+  
+
+      color hdrdefault   #${colors.base0F}   default
+  
+
+      color index        #${colors.base0F}   default    "~D"
+      color index        #${colors.base0A}   default    "~F"
+      color index        #${colors.base0B}   default    "~N"
+      
+
+      color quoted       #${colors.base09}   default
+      color signature    #${colors.base0A}   default
+      
+
+      color body         #${colors.base05}   default    "[:;][-o]?[)/(|]"
+      
+
+      color body         #${colors.base0D}   default    "([a-z][a-z0-9+-]*://(((([a-z0-9_.!~*'();:&=+$,-]|%[0-9a-f][0-9a-f])*@)?((([a-z0-9]([a-z0-9-]*[a-z0-9])?)\\.)*([a-z]([a-z0-9-]*[a-z0-9])?)\\.?|[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+)(:[0-9]+)?)|([a-z0-9_.!~*'()$,;:@&=+-]|%[0-9a-f][0-9a-f])+)(/([a-z0-9_.!~*'():@&=+$,-]|%[0-9a-f][0-9a-f])*(;([a-z0-9_.!~*'():@&=+$,-]|%[0-9a-f][0-9a-f])*)*(/([a-z0-9_.!~*'():@&=+$,-]|%[0-9a-f][0-9a-f])*(;([a-z0-9_.!~*'():@&=+$,-]|%[0-9a-f][0-9a-f])*)*)*)?(\\?([a-z0-9_.!~*'();/?:@&=+$,-]|%[0-9a-f][0-9a-f])*)?(#([a-z0-9_.!~*'();/?:@&=+$,-]|%[0-9a-f][0-9a-f])*)?|(www|ftp)\\.(([a-z0-9]([a-z0-9-]*[a-z0-9])?)\\.)*([a-z]([a-z0-9-]*[a-z0-9])?)\\.?(:[0-9]+)?(/([-a-z0-9_.!~*'():@&=+$,]|%[0-9a-f][0-9a-f])*(;([-a-z0-9_.!~*'():@&=+$,]|%[0-9a-f][0-9a-f])*)*(/([-a-z0-9_.!~*'():@&=+$,]|%[0-9a-f][0-9a-f])*(;([-a-z0-9_.!~*'():@&=+$,]|%[0-9a-f][0-9a-f])*)*)*)?(\\?([-a-z0-9_.!~*'();/?:@&=+$,]|%[0-9a-f][0-9a-f])*)?(#([-a-z0-9_.!~*'();/?:@&=+$,]|%[0-9a-f][0-9a-f])*)?)[^].,:;!)? \t\r\n<>\"]"
+      
+      color body         #${colors.base0D}   default    "((@(([0-9a-z-]+\\.)*[0-9a-z-]+\\.?|#[0-9]+|\\[[0-9]?[0-9]?[0-9]\\.[0-9]?[0-9]?[0-9]\\.[0-9]?[0-9]?[0-9]\\.[0-9]?[0-9]?[0-9]\\]),)*@(([0-9a-z-]+\\.)*[0-9a-z-]+\\.?|#[0-9]+|\\[[0-9]?[0-9]?[0-9]\\.[0-9]?[0-9]?[0-9]\\.[0-9]?[0-9]?[0-9]\\.[0-9]?[0-9]?[0-9]\\]):)?[0-9a-z_.+%$-]+@(([0-9a-z-]+\\.)*[0-9a-z-]+\\.?|#[0-9]+|\\[[0-2]?[0-9]?[0-9]\\.[0-2]?[0-9]?[0-9]\\.[0-2]?[0-9]?[0-9]\\.[0-2]?[0-9]?[0-9]\\])"
+    '';
+
   };
 
   xdg = {
-    desktopEntries = {
-      neomutt = {
-        name = "Neomutt";
-        genericName = "Email Client";
-        comment = "Read and send emails";
-        exec = "neomutt %U";
-        icon = "mutt";
-        terminal = true;
-        categories = [ "Network" "Email" "ConsoleOnly" ];
-        type = "Application";
-        mimeType = [ "x-scheme-handler/mailto" ];
-      };
+    desktopEntries.neomutt = {
+      name = "neomutt";
+      genericName = "Email Client";
+      comment = "Read and mails emails";
+      exec = "TERM=xterm-direct ${alacritty} --class neomutt -e ${neomutt}";
+      icon = "mutt";
+      categories = [ "Network" "Email" "ConsoleOnly" ];
+      type = "Application";
+      mimeType = [ "x-scheme-handler/mailto" ];
     };
     mimeApps.defaultApplications = {
       "x-scheme-handler/mailto" = "neomutt.desktop";
     };
   };
 
+  programs.bash.shellAliases.neomutt = "TERM=xterm-direct ${neomutt}";
 }
