a27ec4d95472f52c3d62f994e0735843da96bdd6
PK <kuehn@peasec.tu-darmstadt.de>
2023-10-04 13:47:07 +0200

    adding mpv to workspace 6

--- home/phil/features/desktop/hyprland/windowrule.nix
+++ home/phil/features/desktop/hyprland/windowrule.nix
@@ -5,14 +5,18 @@
 
       "workspace 2, obsidian"
       "workspace 2, title:Quick Format Citation"
+      "workspace 2, zathura"
 
       "workspace 3, Zotero"
       "workspace 3, libreoffice"
 
+      "workspace 4, neomutt"
       "workspace 4, thunderbird"
-      "workspace 4, signal"
+      "workspace 4, Signal"
 
       "workspace 5, firefox"
+
+      "workspace 6, mpv"
 
       "workspace 9, ncmpcpp"
     ];
