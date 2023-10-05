94504b86db042f4189d9563ecc353ab3272ee2c2
PK <kuehn@peasec.tu-darmstadt.de>
2023-10-05 12:14:59 +0200

    adding cloudsend as pkg but not finished now

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,5 @@
 { pkgs ? import <nixpkgs> { } }: rec {
   #sharetechmono = pkgs.callPackage ./sharetechmono { }; # obsolete due to pkgs.google-fonts
   #tmx = pkgs.callPackage ./tmx { };
+  cloudsend = pkgs.callPackage ./cloudsend { };
 }
