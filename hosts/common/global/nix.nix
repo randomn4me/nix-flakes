868252c2ac4a49b22de2be72f0cef237978a307c
PK <kuehn@peasec.tu-darmstadt.de>
2023-10-13 21:09:32 +0200

    update nix

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -4,6 +4,7 @@
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
       auto-optimise-store = true;
+      warn-dirty = false;
 
       substituters = ["https://hyprland.cachix.org"];
       trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
@@ -11,8 +12,8 @@
 
     gc = {
       automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 3d";
+      dates = "daily";
+      options = "--delete-older-than 7d";
     };
 
     registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
