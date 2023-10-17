b4898b8ad5a208a77d02069dbd93685f3d1cad06
PK <kuehn@peasec.tu-darmstadt.de>
2023-10-17 15:58:25 +0200

    fixing tmux colors

--- home/phil/features/cli/tmux.nix
+++ home/phil/features/cli/tmux.nix
@@ -5,8 +5,7 @@
         clock24 = true;
         extraConfig = let inherit (config.colorscheme) colors; in ''
           # https://gist.github.com/andersevenrud/015e61af2fd264371032763d4ed965b6
-          # set -ag terminal-overrides ",$TERM:RGB"
-          set -g default-terminal "tmux-256color"
+          set -g default-terminal "xterm-direct"
           set -sg terminal-overrides ",*:RGB"
 
           set -gw mode-keys vi
