b078a0fe2bd596110b870b89f8826c14a0d7aa97
PK <kuehn@peasec.tu-darmstadt.de>
2023-11-06 10:40:48 +0100

    disabling powerManagement as it is to intrusive with usb devices

--- hosts/common/optional/powersaving.nix
+++ hosts/common/optional/powersaving.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
-  powerManagement.powertop.enable = true;
+  #powerManagement.powertop.enable = true;
   environment.systemPackages = with pkgs; [ powertop ];
 
   services.auto-cpufreq = {
