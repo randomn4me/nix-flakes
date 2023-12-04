7883cc561fc23a474cb326f66dc1b200e72eb645
PK <kuehn@peasec.tu-darmstadt.de>
2023-12-04 14:35:16 +0100

    mime was not working, fixed

--- home/phil/features/desktop/common/imv.nix
+++ home/phil/features/desktop/common/imv.nix
@@ -1,17 +1,22 @@
 {
   programs.imv.enable = true;
 
-  xdg.desktopEntries = {
-    imv = {
-      name = "imv";
-      genericName = "Image Viewer";
-      comment = "Viewer for Image files";
-      exec = "imv %F";
-      mimeType = [ "image/*" ];
-      type = "Application";
-      categories = [ "Utility" ];
+  xdg = {
+    desktopEntries = {
+      imv = {
+        name = "imv";
+        genericName = "Image Viewer";
+        comment = "Viewer for Image files";
+        exec = "imv %F";
+        mimeType = [ "image/*" ];
+        type = "Application";
+        categories = [ "Utility" ];
+      };
+    };
+
+    mimeApps.defaultApplications = {
+      "image/jpeg" = [ "imv.desktop" ];
     };
   };
 
-  xdg.mimeApps.defaultApplications = { "image/*" = [ "imv.desktop" ]; };
 }
