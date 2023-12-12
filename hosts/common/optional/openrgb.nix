cff610e82a5c2c070b9da2f2004978df329d94d2
PK <kuehn@peasec.tu-darmstadt.de>
2023-12-12 16:48:06 +0100

    fix

--- hosts/common/optional/openrgb.nix
+++ hosts/common/optional/openrgb.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [ openrgb-with-all-plugins ];
+  environment.systemPackages = with pkgs; [ openrgb-with-all-plugins ];
   services.hardware.openrgb.enable = true;
 }
