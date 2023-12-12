d256a4be23c1cae1ad38835fb7b16735e70c0ece
PK <kuehn@peasec.tu-darmstadt.de>
2023-12-12 16:41:29 +0100

    adding more power setting

--- hosts/common/optional/powersaving.nix
+++ hosts/common/optional/powersaving.nix
@@ -1,19 +1,8 @@
 { pkgs, ... }: {
+  powerManagement.enable = true;
+
   powerManagement.powertop.enable = true;
   environment.systemPackages = with pkgs; [ powertop ];
 
-  services.auto-cpufreq = {
-    enable = true;
-
-    settings = {
-      battery = {
-        governor = "powersave";
-        turbo = "never";
-      };
-      charger = {
-        governor = "performance";
-        turbo = "auto";
-      };
-    };
-  };
+  services.thermald.enable = true;
 }
