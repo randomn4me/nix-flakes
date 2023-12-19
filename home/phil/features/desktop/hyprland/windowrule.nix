46048be86d9212119070f58fc17c929ba22c5cee
PK <kuehn@peasec.tu-darmstadt.de>
2023-12-19 11:36:36 +0100

    moving obsidian to ws3

--- home/phil/features/desktop/hyprland/windowrule.nix
+++ home/phil/features/desktop/hyprland/windowrule.nix
@@ -3,11 +3,11 @@
     windowrule = [
       "workspace 1, Alacritty"
 
-      "workspace 2, obsidian"
-      "workspace 2, title:Quick Format Citation"
-
       "workspace 3, Zotero"
       "workspace 3, libreoffice"
+      "workspace 3, obsidian"
+      "workspace 3, title:Quick Format Citation"
+
 
       "workspace 4, neomutt"
       "workspace 4, Signal"
