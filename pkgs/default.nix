c32008d8fb670a2f8e4a8dea165cf4f656b363c1
PK <kuehn@peasec.tu-darmstadt.de>
2024-01-17 10:34:48 +0100

    removed lyrics

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,3 @@
 { pkgs ? import <nixpkgs> { } }: rec {
-  #sharetechmono = pkgs.callPackage ./sharetechmono { }; # obsolete due to pkgs.google-fonts
-  #tmx = pkgs.callPackage ./tmx { };
   cloudsend = pkgs.callPackage ./cloudsend { };
 }
