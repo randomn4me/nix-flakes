03f486a1c18d955f5ceb0134f212d90873f60a2a
Philipp Kühn <git@audacis.net>
2024-01-18 09:27:43 +0100

    update ignore keyboard

--- hosts/common/optional/powersaving-ignore-usb.nix
+++ hosts/common/optional/powersaving-ignore-usb.nix
@@ -1,29 +1,44 @@
 {pkgs, ...}: let
   # source https://askubuntu.com/a/1026527
-  undoPowertopTuningScript = pkgs.writeShellScript "undo-powertop-tuning" ''
+  ignoreKeyboardScript = pkgs.writeShellScript "powersaving-ignore-usb" ''
     #!/bin/sh
 
-      TARGET_DEVICE_NAME="SK622 Mechanical Keyboard - White Edition"
-      HIDDEVICES=$(ls /sys/bus/usb/drivers/usbhid | grep -oE '^[0-9]+-[0-9\.]+' | sort -u)
+    TARGET_DEVICE_NAME="SK622 Mechanical Keyboard - White Edition"
+    HIDDEVICES=$(ls /sys/bus/usb/drivers/usbhid | grep -oE '^[0-9]+-[0-9\.]+' | sort -u)
 
-      for i in $HIDDEVICES; do
-    DEVICE_NAME=$(cat /sys/bus/usb/devices/$i/product)
+    for i in $HIDDEVICES; do
+      DEVICE_NAME=$(cat /sys/bus/usb/devices/$i/product)
       if [ "$DEVICE_NAME" = "$TARGET_DEVICE_NAME" ]; then
-      echo "Enabling $DEVICE_NAME"
-      echo 'on' > /sys/bus/usb/devices/$i/power/control
+        echo "Enabling $DEVICE_NAME"
+        echo 'on' > /sys/bus/usb/devices/$i/power/control
       fi
-      done
+    done
   '';
+
+  serviceName = "ignoreKeyboard";
 in {
-  systemd.services.undo-powertop-tuning = {
-    description = "Undo certain Powertop auto-tunings";
+  systemd.services.${serviceName} = {
+    description = "Ignore keyboard for powertop autotune";
     after = ["powertop.service"];
     requires = ["powertop.service"];
     wantedBy = ["multi-user.target"];
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = undoPowertopTuningScript;
+      ExecStart = ignoreKeyboardScript;
       RemainAfterExit = true;
     };
+  };
+
+  services.udev = {
+    enable = true;
+
+    extraRules = ''
+      ACTION=="add",\
+      SUBSYSTEM=="usb",\
+      ATTRS{idVendor}=="2516",\
+      ATTRS{idProduct}=="014b",\
+      TAG+="systemd",\
+      ENV{SYSTEMD_WANTS}+="${serviceName}.service"
+    '';
   };
 }
--- hosts/common/optional/udev.nix
+++
@@ -1,5 +0,0 @@
-{
-  services.udev.extraRules = ''
-    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="2516", ATTR{idProduct}=="014b", ENV{POWERTOP_IGNORE}="1"
-  '';
-}
