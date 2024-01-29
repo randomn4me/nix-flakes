b4280945387542b132a3f67c6089835efdc02860
Philipp Kühn <git@audacis.net>
2024-01-29 21:33:36 +0100

    removal of xdg

--- home/global/default.nix
+++ home/global/default.nix
@@ -37,23 +37,6 @@ in {
 
   systemd.user.startServices = "sd-switch";
 
-  xdg = {
-    enable = true;
-    userDirs = {
-      enable = true;
-      createDirectories = true;
-
-      desktop = "${config.home.homeDirectory}/tmp";
-      documents = "${config.home.homeDirectory}/usr/docs";
-      download = "${config.home.homeDirectory}/tmp";
-      music = "${config.home.homeDirectory}/usr/music";
-      pictures = "${config.home.homeDirectory}/usr/pics";
-      publicShare = "${config.home.homeDirectory}/var/share";
-      templates = "${config.home.homeDirectory}/var/templates";
-      videos = "${config.home.homeDirectory}/usr/vids";
-    };
-  };
-
   colorscheme = lib.mkDefault colorSchemes.rose-pine;
   home.file.".colorscheme".text = config.colorscheme.slug;
   home.file.".colorscheme-colors".text = builtins.concatStringsSep "\n"
