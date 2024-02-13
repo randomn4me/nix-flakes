7c7d2d3826f30d953dd104ab81bf9a70c69486b6
Philipp Kühn <git@audacis.net>
2024-02-13 14:16:23 +0100

    removing ankisyncd

--- hosts/hetzner/default.nix
+++ hosts/hetzner/default.nix
@@ -9,7 +9,7 @@
     ../common/optional/services/mastodon.nix
     ../common/optional/services/taskserver.nix
     ../common/optional/services/vaultwarden.nix
-    #../common/optional/services/anki.nix
+    ../common/optional/services/gitea.nix
   ];
 
   networking.hostName = "hetzner";
