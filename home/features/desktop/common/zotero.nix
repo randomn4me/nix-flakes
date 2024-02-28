a64b876cf1b0dd146dd2367df98f099c4cae71b5
Philipp Kühn <git@audacis.net>
2024-02-28 21:23:17 +0100

    removing headless zotero

--- home/features/desktop/common/zotero.nix
+++ home/features/desktop/common/zotero.nix
@@ -2,11 +2,11 @@
 {
     home.packages = with pkgs; [ zotero ];
 
-    systemd.user.services.zotero = {
-        Unit.Description = "zotero";
-        Service.ExecStart = "${pkgs.zotero}/bin/zotero --headless";
-        Install.WantedBy = [ "default.target" ];
-    };
+    #systemd.user.services.zotero = {
+    #    Unit.Description = "zotero";
+    #    Service.ExecStart = "${pkgs.zotero}/bin/zotero --headless";
+    #    Install.WantedBy = [ "default.target" ];
+    #};
 
 }
 
