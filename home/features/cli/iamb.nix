7ba680849e4059ac2097f8d444c5cd65d6429bf6
Philipp Kühn <git@audacis.net>
2024-02-28 21:13:26 +0100

    adding iamb

---
+++ home/features/cli/iamb.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [ iamb ];
+
+  xdg.configFile."iamb/iamb.conf".text = /* json */ ''
+  {
+    "profiles": {
+      "private": {
+        "url": "https://matrix.org",
+        "user_id": "@r4ndomname:matrix.org"
+      },
+        "work": {
+          "url": "https://matrix.tu-darmstadt.de",
+          "user_id": "@ba01viny:matrix.tu-darmstadt.de"
+        }
+    },
+    "default_profile": "private",
+    "layout": { "style": "restore" },
+    "settings": {
+      "username_display": "localpart",
+    }
+  }
+  '';
+}
