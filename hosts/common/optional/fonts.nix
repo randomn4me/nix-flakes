00e189f5253a5625ae03750e2249b9d8a80e78e0
Philipp Kühn <git@audacis.net>
2024-03-01 21:53:40 +0100

    fixing fonts

--- hosts/common/optional/fonts.nix
+++ hosts/common/optional/fonts.nix
@@ -7,6 +7,7 @@
       roboto-serif
       inconsolata-nerdfont
       vistafonts
+      noto-fonts-color-emoji
   
       (pkgs.google-fonts.override { fonts = [ "ShareTechMono" ]; })
   
@@ -19,6 +20,7 @@
         serif = [ "Fira Sans" ];
         sansSerif = [ "Fira Sans" ];
         monospace = [ "Inconsolata Nerd Font Mono" "Noto Color Emoji" ];
+        emoji = [ "Noto Color Emoji" ];
       };
     };
   };
