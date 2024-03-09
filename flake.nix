672991458c0a8cdaeb30978bfc1d0e8e1a0b18ce
Philipp Kühn <git@audacis.net>
2024-03-09 10:02:38 +0100

    input nixvim

--- flake.nix
+++ flake.nix
@@ -32,6 +32,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     nix-index-database.url = "github:Mic92/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
   };
