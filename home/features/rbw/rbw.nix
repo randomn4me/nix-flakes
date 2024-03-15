695ba96e5b4fb63ed5115357f91d34735edd621e
Philipp Kühn <git@audacis.net>
2024-03-15 21:01:47 +0100

    fix rbw

--- home/features/rbw/rbw.nix
+++ home/features/rbw/rbw.nix
@@ -1,10 +1,11 @@
+{ pkgs, ... }:
 {
   programs.rbw = {
     enable = true;
     settings = {
       email = "p.services@audacis.net";
       base_url = "https://vault.audacis.net";
-      pinentry = "qt";
+      pinentry = pkgs.pinentry-bemenu;
     };
   };
 }
