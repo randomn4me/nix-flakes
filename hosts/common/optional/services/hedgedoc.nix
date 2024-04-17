1aa173dfcdf0a664cb6976b33a29ea55ada3b7eb
Philipp Kühn <git@audacis.net>
2024-04-17 16:19:51 +0200

    prepare hedgedoc use

---
+++ hosts/common/optional/services/hedgedoc.nix
@@ -0,0 +1,11 @@
+{
+  # TODO: finalize before use
+  services.hedgedoc = {
+    enable = true;
+    settings = {
+      host = "0.0.0.0";
+      domain = "md";
+      protocolUseSSL = true;
+    };
+  };
+}
