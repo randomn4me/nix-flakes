bd8c424899a3801535daf88d908c2d526745369c
Philipp Kühn <git@audacis.net>
2024-06-24 16:34:54 +0200

    try to fix headset bluetooth profile settings

--- hosts/common/optional/pipewire.nix
+++ hosts/common/optional/pipewire.nix
@@ -1,9 +1,25 @@
 {
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
 
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+
+    wireplumber.extraConfig = {
+      "monitor.bluez.properties" = {
+        "bluez5.enable-sbc-xq" = true;
+        "bluez5.enable-msbc" = true;
+        "bluez5.enable-hw-volume" = true;
+        "bluez5.roles" = [
+          "hsp_hs"
+          "hsp_ag"
+          "hfp_hf"
+          "hfp_ag"
+        ];
+      };
+    };
+
   };
 }
