65f383665288421b2fd1ac288b2e9496337ba0ca
Philipp Kühn <git@audacis.net>
2024-08-29 22:34:07 +0200

    removing taskserver

--- hosts/hetzner/default.nix
+++ hosts/hetzner/default.nix
@@ -7,7 +7,6 @@
 
     ../common/optional/services/fail2ban.nix
     ../common/optional/services/mastodon.nix
-    ../common/optional/services/taskserver.nix
     ../common/optional/services/vaultwarden.nix
     ../common/optional/services/hedgedoc.nix
     ../common/optional/services/forgejo.nix
