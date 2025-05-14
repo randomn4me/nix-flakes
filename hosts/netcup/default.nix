5d1d33184fa3d3313691eabe8756a5b2a1d8376a
Philipp Kühn <git@audacis.net>
2025-05-14 06:31:42 +0200

    merging and adding netcup config

--- hosts/netcup/default.nix
+++ hosts/netcup/default.nix
@@ -8,11 +8,10 @@
     ../common/optional/services/fail2ban.nix
     ../common/optional/services/vaultwarden.nix
     ../common/optional/services/forgejo.nix
-    ../common/optional/services/taskserver.nix
   ];
 
   networking = {
-    hostName = "hetzner";
+    hostName = "netcup";
 
     firewall = {
       enable = true;
@@ -34,21 +33,12 @@
         PermitRootLogin = "no";
       };
     };
+    # responsible for a safe shutdown of your system, among other features utilized by the SCP
+    qemuGuest.enable = true;
 
     journald.extraConfig = "SystemMaxUse=100M";
   };
 
-  # hetzner specific network settings
-  systemd.network = {
-    enable = true;
-
-    networks."10-wan" = {
-      matchConfig.Name = "ens3"; # either ens3 (amd64) or enp1s0 (arm64)
-      networkConfig.DHCP = "ipv4";
-      address = [ "2a01:4f8:c2c:70e3::1/64" ];
-      routes = [ { Gateway = "fe80::1"; } ];
-    };
-  };
-
-  system.stateVersion = "23.11";
+  system.stateVersion = "24.11";
 }
+
