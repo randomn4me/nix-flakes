d03752072994e9ce1234aded343d00f117520ba3
Philipp Kühn <git@audacis.net>
2025-07-04 22:52:57 +0200

    chore: adjusted to rename

--- home/features/desktop/common/wayland-wm/mako.nix
+++ home/features/desktop/common/wayland-wm/mako.nix
@@ -5,19 +5,21 @@ in
 {
   services.mako = {
     enable = true;
-    anchor = "bottom-right";
-    defaultTimeout = 8000;
-    layer = "overlay";
+    settings = {
+      anchor = "bottom-right";
+      defaultTimeout = 8000;
+      layer = "overlay";
 
-    width = 400;
-    height = 150;
+      width = 400;
+      height = 150;
 
-    borderSize = 2;
-    borderRadius = 5;
+      borderSize = 2;
+      borderRadius = 5;
 
-    font = "monospace 12";
-    textColor = "#${colors.base05}";
-    backgroundColor = "#010101";
-    borderColor = "#B1252E";
+      font = "monospace 12";
+      textColor = "#${colors.base05}";
+      backgroundColor = "#010101";
+      borderColor = "#B1252E";
+    };
   };
 }
