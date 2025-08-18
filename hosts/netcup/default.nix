468baa7914349d5dd9ace1120b5f068d25c205b5
Philipp Kühn <git@audacis.net>
2025-08-18 16:24:47 +0200

    feat: adding ssh pub

--- hosts/netcup/default.nix
+++ hosts/netcup/default.nix
@@ -8,6 +8,7 @@
 
     inputs.disko.nixosModules.disko
     ./disko.nix
+
     ../common/optional/services/fail2ban.nix
   ];
 
@@ -21,12 +22,17 @@
 
   nix.gc.dates = "daily";
 
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKluBaVeaUkcr8U5ZF5YTlsyjfUCG0lQkfWrzKVbzM6y"
+  ];
+
   services = {
     qemuGuest.enable = true;
     openssh = {
       enable = true;
       settings = {
-        PermitRootLogin = "no";
+        PasswordAuthentication = "no";
+        PermitRootLogin = "yes";
       };
     };
     journald.extraConfig = "SystemMaxUse=100M";
